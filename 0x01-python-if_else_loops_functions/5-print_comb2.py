a72c504b25c8795eb7dfae47ed532fa5d200bcf4
Viestar <m.silverbuckson@gmail.com>
2023-05-10 04:41:28 +0300

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 for number in range(00, 100):
-
-    print(f"{number:d}", end=" ")
+    print("{}".format(number), end="")
     if number != 99:
-        print(",", end=" ")
+        print(",", end="")
