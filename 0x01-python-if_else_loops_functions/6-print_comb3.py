c758ca257300e733b8eba4994c3e757a16a0e824
Viestar <m.silverbuckson@gmail.com>
2023-05-10 04:54:45 +0300

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -5,6 +5,6 @@ while num < 90:
         num += 1 + num // 10
     print("{:02d}".format(num), end=" ")
     if num != 89:
-        print(",", end=" ")
+        print(",", end="")
     num = num + 1
 print()
