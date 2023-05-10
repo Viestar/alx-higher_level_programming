2425e2952a3dc6a6c8db365adf632f23f26d0ef9
Viestar <m.silverbuckson@gmail.com>
2023-05-10 05:04:35 +0300

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 num = 0
-while num < 90:
+while num < 89:
     if num % 10 == 0:
         num += 1 + num // 10
-    print("{:02d}".format(num), end=" ")
+    print("{:02d}".format(num), end="")
     if num != 89:
-        print(",", end="")
+        print(", ", end="")
     num = num + 1
 print()
