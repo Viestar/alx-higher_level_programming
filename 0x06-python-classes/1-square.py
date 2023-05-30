e252cddc4e67cb97b672ce507c99440e69fe193e
Viestar <m.silverbuckson@gmail.com>
2023-05-31 02:49:44 +0300

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 
+""" Class Definition"""
+
+
 class Square:
+	""" Class Square """
 
     def __init__(self, size):
+        """ initialisation 
+        Args:
+        size (int): New square size
+        """
         self.__size = size
