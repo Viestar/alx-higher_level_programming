42eaa764f32525526d37713e1394c2aafa2bc896
Viestar <m.silverbuckson@gmail.com>
2023-05-31 03:00:52 +0300

    4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,15 +1,26 @@
 #!/usr/bin/python3
 
+""" Class Definition"""
+
+
 class Square:
+	"""Square Class """
+
 	def __init__(self, size=0):
+		""" initialisation
+		Args:
+		size (int): New square size
+		"""
 		self.size = size
 
 	@property
 	def size(self):
-        return (self.__size)
+		""" Get/set square current size """
+		return (self.__size)
 
 	@size.setter
     def size(self, value):
+		""" Raises errors if cannot assign private size"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -17,4 +28,5 @@ class Square:
         self.__size = value
 
     def area(self):
+		""" Returns current square area """
         return (self.__size * self.__size)
