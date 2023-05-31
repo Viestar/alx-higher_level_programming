23563b18d2df0d7653099d26021002c4141761f3
Viestar <m.silverbuckson@gmail.com>
2023-05-31 03:10:52 +0300

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,16 +1,28 @@
 #!/usr/bin/python3
 
+""" Class Definition"""
+
+
 class Square:
+	"""Square Class """
+
     def __init__(self, size=0):
+		""" initialisation
+		Args:
+		size (int): New square size
+		position (int): nEW SUARE position
+		"""
         self.size = size
         self.position = position
 
     @property
     def size(self):
+		""" Get/set square current size """
         return (self.__size)
 
     @size.setter
     def size(self, value):
+		""" Raises errors if cannot assign private size"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -19,10 +31,12 @@ class Square:
 
     @property
     def position(self):
+        """ Get/set current position """
         return (self.__position)
 
     @position.setter
     def position(self, value):
+        """ Reports errors """
         if (not isinstance(value, tuple) or
                 len(value) != 2 or
                 not all(isinstance(index, int) for index in value) or
@@ -31,9 +45,11 @@ class Square:
         self.__position = value
 
     def area(self):
+       """ Returns the area"""
         return (self.__size * self.__size)
 
     def my_print(self):
+        """ Prints a square with a symbol """
         for i in range(0, self.__size):
             [print("#", end="") for j in range(self.__size)]
             print()
