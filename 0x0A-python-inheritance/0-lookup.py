e708ac1599710da3afdc85dfe93e54b6f9016fea
Viestar <m.silverbuckson@gmail.com>
2023-06-12 11:01:39 +0300

    0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Function Documentation: lookup()"""
+
+
+def lookup(obj):
+    """Returns a list of all available attributes"""
+    return dir(obj)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,4 @@
+# 0x0A. Python - Inheritance
+
+## 0-lookup.py
+
