e48a6c5125ab94a75973448413bbf73f9814de3f
Viestar <m.silverbuckson@gmail.com>
2023-06-14 01:22:27 +0300

    0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Documentation for read file function """
+
+
+def read_file(filename=""):
+    """Reads and prints a file to the standard output"""
+
+    with open(filename, encoding='utf-8') as mideh:
+        print(mideh.read(), end="")
