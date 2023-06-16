1197408efe0711dfac5569a4ed9f34885cbb57c9
Viestar <m.silverbuckson@gmail.com>
2023-06-16 12:45:27 +0300

    4-from_json_string.py

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Documentation for from_json_string """
+
+
+import json
+
+
+def from_json_string(my_str):
+    """ returns an object(python data structure) 
+
+    Args: my_str (JSON string): json string.
+    Return: object, the python data structure
+    """
+    object = json.load(my_str)
+    return object
