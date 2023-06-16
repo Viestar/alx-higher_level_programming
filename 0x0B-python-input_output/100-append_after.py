ce0038a583a648d3a0f0273003869b9a0d82250e
Viestar <m.silverbuckson@gmail.com>
2023-06-16 14:36:51 +0300

    100-append_after.py

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Documentation for append_after function"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """ Appends new_string after the search_string 
+
+    Args:
+    filename (file): File to be appended
+    new_string: string to be added
+    search_string: indicates where to insert
+    Return: new file
+
+    """
+
+    with open(filename, "r+", encoding='utf-8') as mideh:
+
+        for current_string in mideh:
+            if search_string in current_string:
+                mideh.write(new_string)
+            else:
+                continue
+    return mideh
