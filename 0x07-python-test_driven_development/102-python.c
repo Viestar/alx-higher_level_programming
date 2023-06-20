7d1f75587ffd78b904075a60d931a6a4116c58a8
Viestar <m.silverbuckson@gmail.com>
2023-06-20 23:20:39 +0300

    102-python.c

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -2,24 +2,25 @@
 
 void print_python_string(PyObject *p)
 {
-    /* Check if a string is valid */
     if (!PyUnicode_Check(p))
     {
-        printf("Usage: Invalid string.\n");
+        printf("Error: Invalid string object.\n");
         return;
     }
-    Py_ssize_t length = PyUnicode_GET_LENGTH(p);
-    Py_UCS4 *unicode = PyUnicaode_AsUCS4Copy(p);
 
+    Py_ssize_t length = PyUnicode_GET_LENGTH(p);
+    Py_UCS4 *unicode = PyUnicode_AsUCS4Copy(p);
     if (!unicode)
     {
-        printf("Error: Failed to retrieve string data\n");
+        printf("Error: Failed to retrieve string data.\n");
         return;
     }
-    printf("String: ") for (Py_ssize_t i = 0; i < length; i++)
+
+    printf("String: ");
+    for (Py_ssize_t i = 0; i < length; i++)
     {
-        Py_UCS4 chaar = unicode[i];
-        printf("%lc", chaar);
+        Py_UCS4 ch = unicode[i];
+        printf("%lc", ch);
     }
     printf("\n");
 
