e1670bc863a2ef95a3ca2203e2464da5f4a1035f
Viestar <m.silverbuckson@gmail.com>
2023-06-20 10:56:21 +0300

    Removed unnecessary float exception

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -12,11 +12,7 @@ def add_integer(a, b=98):
 
     if not isinstance(a, (int)):
         raise TypeError("a must be an integer")
-    if not isinstance(a, (float)):
-        raise TypeError("a must be an integer")
     if not isinstance(b, (int)):
-        raise TypeError("b must be an integer")
-    if not isinstance(b, (float)):
         raise TypeError("b must be an integer")
     if isinstance(a, float):
         a = int(a)
