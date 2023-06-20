c48983e646261e53e7e978bbb6a4da343d783632
Viestar <m.silverbuckson@gmail.com>
2023-06-20 22:02:46 +0300

    101-lazy_matrix_mul.py

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" LAzy matrix Documentation"""
+import numpy as num
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """ Multiplies two matrices using numpy module """
+    matrix_one = num.array(m_a)
+    matrix_two = num.array(m_b)
+
+    product = num.dot(matrix_one, matrix_two)
+
+    return product.tolist()
