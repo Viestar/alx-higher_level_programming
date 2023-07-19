b3d9f5e4e052141c4d1611188f8d28400176ec6f
Viestar <m.silverbuckson@gmail.com>
2023-07-19 04:53:06 +0300

    15-groups.sql++

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
 -- lists the number of records with the same score in the second_table of the database.
 SELECT score, COUNT(*) AS number
-FROM hbtn_0c_0.second_table
+FROM {0}.second_table
 GROUP BY score
 ORDER BY number DESC;
