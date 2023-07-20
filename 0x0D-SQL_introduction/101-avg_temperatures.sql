dd88cd7474d38f57803167f2f1db0aa3dd36e6b3
Viestar <m.silverbuckson@gmail.com>
2023-07-21 00:10:47 +0300

    101-avg_temperatures.sql

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,6 +1,6 @@
 -- displays descending average of value(temperature) by city
 SELECT city,
     AVG(value) AS av_tmp
-FROM temperatures
+FROM hbtn_0c_0.temperatures
 GROUP BY city
 ORDER BY av_tmp DESC;
