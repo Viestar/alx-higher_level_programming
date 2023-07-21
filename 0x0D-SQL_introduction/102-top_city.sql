579fc072a4bcb00527fce1d590d0e90dd2d79c8d
Viestar <m.silverbuckson@gmail.com>
2023-07-21 09:11:31 +0300

    102-top_city.sql

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,8 +1,8 @@
 -- Retrieves only top 3 using LIMIT
 SELECT `city`,
-    AVG(`value`) AS `average_temperature`
+    AVG(`value`) AS `avg_temp`
 FROM `temperatures`
-WHERE MONTH(date) IN (7, 8)
+WHERE MONTH IN (7, 8)
 GROUP BY `city`
-ORDER BY `average_temperature` DESC
+ORDER BY `avg_temp` DESC
 LIMIT 3;
