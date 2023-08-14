325966cfe277f0aebd9882679d12e6407f43df19
Viestar <m.silverbuckson@gmail.com>
2023-08-14 22:51:02 +0300

    6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// A script that prints "C is fun", "Python is Cool" and "JavaScript is amazing" on different lines
+let languages = ["C is fun", "Python is cool", "JavaScript is amazing"]
+let index = 0
+while (index < languages.length) {
+    console.log(languages[index]);
+    index++;
+}
