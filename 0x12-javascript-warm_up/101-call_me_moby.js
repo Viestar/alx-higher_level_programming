e2a14c322e06016ccb963d666b9114dbc0283f3b
Viestar <m.silverbuckson@gmail.com>
2023-08-14 23:44:49 +0300

    101-call_me_moby.js

---
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// executes x times a function.
+let index = 0
+exports.callMeMoby = function (x, theFunction) {
+    while (index < x) {
+        theFunction();
+        index++;
+    };
+};
