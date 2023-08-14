2c2babfe011fd693135b7dfd908728b214cc35e2
Viestar <m.silverbuckson@gmail.com>
2023-08-14 22:32:38 +0300

    1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 // A script that prints "C is fun", "Python is Cool" and "JavaScript is amazing" on different lines
-console.log("C is fun\nPython is Cool\navaScript is amazing");
+console.log("C is fun\nPython is cool\nJavaScript is amazing");
