8a1bd580f8e566947001b128ed19723f1aa15f01
Viestar <m.silverbuckson@gmail.com>
2023-08-14 23:24:45 +0300

    9-add.js

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// A script that prints the addition of 2 integers
+
+let args = process.argv;
+const a = args[2];
+const b = args[3];
+function add(a, b) {
+    if (isNaN(a) || isNaN(b)) {
+        return (NaN);
+    } else {
+        return (parseInt(a) + parseInt(b)); // Conversion
+    }
+};
+console.log(add(a, b));
