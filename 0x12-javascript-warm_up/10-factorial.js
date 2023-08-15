7255c11d203cc878bc9d32633a9143d1fb00a396
Viestar <m.silverbuckson@gmail.com>
2023-08-15 10:05:02 +0300

    Added space before function parenthesis

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
-// A script that prints the addition of 2 integers
 // a script that computes and prints a factorial
 
 const number = process.argv[2];
 function factorial(number) {
-    if (isNaN(number) || number === 1) {
-        return (1);
-    } else {
-        return (number * factorial(number - 1));
-    }
-};
+  if (isNaN(number) || number === 1) {
+    return (1);
+  } else {
+    return (number * factorial(number - 1));
+  }
+}
 console.log(factorial(parseInt(number)));
