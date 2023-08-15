7a92eac7d05bc9de084bd7112e9cbfb186ce3518
Viestar <m.silverbuckson@gmail.com>
2023-08-15 08:51:37 +0300

    Indented 2 spaces, removed double quotes

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 /*
-A script that prints My number: <first argument 
+A script that prints My number: <first argument
 converted in integer > if the first argument can be converted to an integers
 */
 const args = process.argv;
 const number = parseInt(args[2], 10);
 
 if (isNaN(number)) {
-    console.log('Not a number');
+  console.log('Not a number');
 } else {
-    console.log(`My number: ${number}`);
+  console.log(`My number: ${number}`);
 }
