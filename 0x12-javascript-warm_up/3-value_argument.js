d01cf540f43796d43282d40a4d893eef02cb977e
Viestar <m.silverbuckson@gmail.com>
2023-08-15 08:35:33 +0300

    semi-standard - added single quotes

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,9 +2,7 @@
 // A script that prints the first argument passed.
 const args = process.argv[2]
 if (args === undefined) {
-    console.log("No argument")
+    console.log('No argument')
 } else {
     console.log(args);
 }
-
-
--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -2,5 +2,3 @@
 // A script that prints two arguments passed to it, joined by “ is ”;.
 const args = process.argv;
 console.log(`${args[2]} is ${args[3]}`);
-
-
