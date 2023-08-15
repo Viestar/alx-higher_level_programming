a69af0a202be4aeff981d85101f93cfc35a2bfa8
Viestar <m.silverbuckson@gmail.com>
2023-08-15 08:47:04 +0300

    Indented 2 spaces, removed double quotes and irrelevant semi-colon

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // A script that prints the first argument passed.
-const args = process.argv[2]
+const args = process.argv[2];
 if (args === undefined) {
   console.log('No argument');
 } else {
