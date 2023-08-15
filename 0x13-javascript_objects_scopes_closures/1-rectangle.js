a2de57df3dbe6098bc16309d0a57ea9bbeec45ce
Viestar <m.silverbuckson@gmail.com>
2023-08-15 15:55:59 +0300

    Turned to a module

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -2,9 +2,9 @@
 
 // Rectangle class with a constructor intitialising height and width
 
-class Rectangle {
-    constructor(w, h) {
-        this.w = width;
-        this.h = height;
-    }
-}
+module.exports = class Rectangle {
+  constructor(w, h) {
+    this.w = width;
+    this.h = height;
+  }
+};
