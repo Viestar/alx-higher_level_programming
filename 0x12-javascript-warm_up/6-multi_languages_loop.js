81094aa7de71bcabf7ecd40656e9b391a68dac40
Viestar <m.silverbuckson@gmail.com>
2023-08-15 09:02:44 +0300

    Indented 2 spaces, removed double quotes

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -3,6 +3,6 @@
 const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let index = 0;
 while (index < languages.length) {
-    console.log(languages[index]);
-    index++;
+  console.log(languages[index]);
+  index++;
 }
