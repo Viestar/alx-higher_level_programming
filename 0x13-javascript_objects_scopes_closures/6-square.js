558939de2251d971346f4f00e2e73fa8c3106603
Viestar <m.silverbuckson@gmail.com>
2023-08-15 23:38:03 +0300

    Renamed Square to diff from square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -8,7 +8,7 @@ module.exports = class Square extends Squared {
       this.print();
     } else {
       for (let i = 0; i < this.width; i++) {
-        console.log('c'.repeat(this.height));
+        console.log('C'.repeat(this.height));
       }
     }
   }
