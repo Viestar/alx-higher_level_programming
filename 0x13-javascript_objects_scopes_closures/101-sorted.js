0de93855e112e92b39809e8aa391e0e307bfa798
Viestar <m.silverbuckson@gmail.com>
2023-08-16 00:43:31 +0300

    101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 // imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence.
-const dataModule = require('./101-data'); // Update the path accordingly
+const dict = require('./101-data'); // Update the path accordingly
 
-const initialDict = dataModule.dict;
+const initialDict = dict.dict;
 const newUserDict = {};
 
 // Create the new dictionary of user ids by occurrence
 for (const userId in initialDict) {
   const occurrence = initialDict[userId];
-  if (!newUserDict.hasOwnProperty(occurrence)) {
+  if (!Object.prototype.hasOwnProperty.call(newUserDict, occurrence)) {
     newUserDict[occurrence] = [];
   }
   newUserDict[occurrence].push(userId);
