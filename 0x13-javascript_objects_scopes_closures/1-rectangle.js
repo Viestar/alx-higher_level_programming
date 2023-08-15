ec22da24ea85db70eb81faa40a47110ee1c93ca6
Viestar <m.silverbuckson@gmail.com>
2023-08-15 15:43:21 +0300

    1-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// Rectangle class with a constructor intitialising height and width
+
+class Rectangle {
+    constructor(w, h) {
+        this.w = width;
+        this.h = height;
+    }
+}
