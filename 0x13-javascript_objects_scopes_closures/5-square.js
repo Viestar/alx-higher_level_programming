f2632c37de6ae6402bd617024d1e7b95711fd448
Viestar <m.silverbuckson@gmail.com>
2023-08-15 21:16:49 +0300

    Corrected mistake in module

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -2,7 +2,7 @@
 
 // A class Square that defines a square and inherits from Rectangle of 4-rectangle.js:
 const Rectangle = require('./4-rectangle');
-modulue.exports = class Square extends Rectangle {
+module.exports = class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
