986e5bd54d86b257fb2e74af745c691a7b4aad5f
Viestar <m.silverbuckson@gmail.com>
2023-08-15 08:59:28 +0300

    Indented 2 spaces, removed double quotes

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // A script that prints "C is fun", "Python is Cool" and "JavaScript is amazing" on different lines
-let languages = ["C is fun", "Python is cool", "JavaScript is amazing"]
-let index = 0
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let index = 0;
 while (index < languages.length) {
     console.log(languages[index]);
     index++;
