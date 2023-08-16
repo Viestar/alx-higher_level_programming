537e56cbd4ab7b161165c2916a1512e2c6f2f8da
Viestar <m.silverbuckson@gmail.com>
2023-08-16 10:24:14 +0300

    Removed Titles

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -5,5 +5,5 @@ const dataModule = require('./100-data'); // Update the path accordingly
 const initialList = dataModule.list;
 const newList = initialList.map((value, index) => value * index);
 
-console.log('Initial List:', initialList);
-console.log('New List:', newList);
+console.log(initialList);
+console.log(newList);
