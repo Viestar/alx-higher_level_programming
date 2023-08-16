7560c21d24acf179fa497eb0be3c69fcdb95e1bb
Viestar <m.silverbuckson@gmail.com>
2023-08-16 12:28:51 +0300

    102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,26 +1,16 @@
 #!/usr/bin/node
 // imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence.
-const fs = require('fs');
-const path = require('path');
 
-if (process.argv.length !== 5) {
-  console.error('Usage: node concatFiles.js <source-file1> <source-file2> <destination-file>');
-  process.exit(1);
-}
+const fs = require('fs');
 
 const sourceFilePath1 = process.argv[2];
 const sourceFilePath2 = process.argv[3];
 const destinationFilePath = process.argv[4];
 
-try {
-  const content1 = fs.readFileSync(sourceFilePath1, 'utf8');
-  const content2 = fs.readFileSync(sourceFilePath2, 'utf8');
+const content1 = fs.readFileSync(sourceFilePath1, 'utf8');
+const content2 = fs.readFileSync(sourceFilePath2, 'utf8');
 
-  const concatenatedContent = content1 + content2;
+const concatenatedContent = content1 + content2;
 
-  fs.writeFileSync(destinationFilePath, concatenatedContent, 'utf8');
-
-  console.log(`Files "${path.basename(sourceFilePath1)}" and "${path.basename(sourceFilePath2)}" concatenated and saved to "${path.basename(destinationFilePath)}".`);
-} catch (error) {
-  console.error('An error occurred:', error.message);
-}
+fs.writeFileSync(destinationFilePath, concatenatedContent, 'utf8');
+console.log(concatenatedContent);
