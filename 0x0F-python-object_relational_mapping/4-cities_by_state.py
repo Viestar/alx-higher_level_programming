43115e486802ef5cf06c227d0c623eb8f9a75ac2
Viestar <m.silverbuckson@gmail.com>
2023-08-18 23:25:51 +0300

    Results specificity

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -10,6 +10,7 @@ if __name__ == "__main__":
         db = Qdb.connect(host="localhost", port=3306,
                          user=argv[1], passwd=argv[2], db=argv[3])
         curSor = db.cursor()
-        curSor.execute("SELECT * FROM `cities` INNER JOIN `states` \
+        curSor.execute("SELECT `cities`.`id`, `cities`.`name`, \
+                       `states`.`name` FROM `cities` INNER JOIN `states` \
         ON `cities`.`state_id` = `states`.`id` ORDER BY `cities`.`id` ASC")
         [print(city) for city in curSor.fetchall()], curSor.close(), db.close()
