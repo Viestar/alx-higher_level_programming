d9dd70c80b632154daf78b4f3f76144c4cbcc303
Viestar <m.silverbuckson@gmail.com>
2023-08-18 08:36:32 +0300

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
+""" Usage: ./0-select_states.py <mysql username> <mysql password> <database name> """
+
 from sys import argv
 import sys
 import MySQLdb as Qdb
 
 if __name__ == "__main__":
-    if len(argv) == 4:
-        db = Qdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
-    else:
-        print("Hello worlds")
-        sys.exit(1)
-    curSor = db.cursor()
-    curSor.execute("SELECT * FROM `states` ORDER BY `id` ASC")
-    [print(state) for state in curSor.fetchall()]
-        
+	if len(argv) == 4:
+		db = Qdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+	else:
+		sys.exit(1)
+	curSor = db.                             ()
+	curSor.execute("SELECT * FROM `states` ORDER BY `id` ASC")
+	[print(state) for state in curSor.fetchall()], curSor.close(), db.close()
+		
