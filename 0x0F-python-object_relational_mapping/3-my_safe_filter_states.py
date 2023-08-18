c7e4352e6c82c1ad6e0f2d9f0f5d15b0fb4fe220
Viestar <m.silverbuckson@gmail.com>
2023-08-18 20:11:44 +0300

    3-my_safe_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Usage: ./0-select_states.py <username> <password> <database> <search> """
+
+from sys import argv
+import sys
+import MySQLdb as Qdb
+
+if __name__ == "__main__":
+    if len(argv) == 5:
+        db = Qdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+        curSor = db.cursor()
+        query = "SELECT * FROM `states` WHERE `name`= %s \
+                       ORDER BY `id` ASC".format(argv[4])
+        params = (argv[4])
+        curSor.execute(query, params)
+        [print(state) for state in curSor.fetchall() if state[1] == argv[4]]
+        curSor.close(), db.close()
+    else:
+        sys.exit(1)
--- 0x0F-python-object_relational_mapping/README.md
+++ 0x0F-python-object_relational_mapping/README.md
@@ -6,3 +6,4 @@ Project Code | Project Name | Description
 [0x00] | [Get all states](./0-select_states.py) | A  script that lists all states from the database hbtn_0e_0_usa
 [0x01] | [Filter states](./1-filter_states.py) | A  script that lists all states from the database hbtn_0e_0_usa that start with letter "N"
 [0x02] | [Filter states by user input](./2-my_filter_states.py) | A  script that lists all states from the database hbtn_0e_0_usa where name is the last argument passed.
+[0x03] | [Filter states by user input](./3-my_safe_filter_states.py) | An SQL injection proof script that lists all states from the database hbtn_0e_0_usa where name is the last argument passed .
