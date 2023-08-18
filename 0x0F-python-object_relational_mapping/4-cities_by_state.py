2cea5180c87ff514c3015cc1d018a18d7fc39e18
Viestar <m.silverbuckson@gmail.com>
2023-08-18 23:20:54 +0300

    Joined Tables

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -9,8 +9,7 @@ if __name__ == "__main__":
     if (len(argv) == 4):
         db = Qdb.connect(host="localhost", port=3306,
                          user=argv[1], passwd=argv[2], db=argv[3])
-        query = "SELECT * FROM `cities` INNER JOIN `states` \
-        ON cities.state_id = states.id ORDER BY `cities`.`id` ASC"
         curSor = db.cursor()
-        curSor.execute(query)
+        curSor.execute("SELECT * FROM `cities` INNER JOIN `states` \
+        ON `cities`.`state_id` = `states`.`id` ORDER BY `cities`.`id` ASC")
         [print(city) for city in curSor.fetchall()], curSor.close(), db.close()
