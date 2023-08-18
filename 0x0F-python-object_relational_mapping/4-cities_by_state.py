6e2a7af23601c9900df17e9eb40f3102d4ef773e
Viestar <m.silverbuckson@gmail.com>
2023-08-18 23:30:41 +0300

    Results specificity added system exit

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -10,7 +10,9 @@ if __name__ == "__main__":
         db = Qdb.connect(host="localhost", port=3306,
                          user=argv[1], passwd=argv[2], db=argv[3])
         curSor = db.cursor()
-        curSor.execute("SELECT `c`.`id`, `c`.`name`, `s`.`name` \
-                 FROM `cities` as `c` INNER JOIN `states` as `s` \
-                   ON `c`.`state_id` = `s`.`id` ORDER BY `c`.`id`")
+        curSor.execute("SELECT cities.id, cities.name, states.name \
+            FROM cities JOIN states ON cities.state_id = states.id \
+            ORDER BY cities.id ASC")
         [print(city) for city in curSor.fetchall()], curSor.close(), db.close()
+    else:
+        sys.exit(1)
