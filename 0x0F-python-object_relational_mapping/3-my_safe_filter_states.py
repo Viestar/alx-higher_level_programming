26062baefca879c6f986304eaea0411100886fc9
Viestar <m.silverbuckson@gmail.com>
2023-08-18 20:57:32 +0300

    3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
         db = Qdb.connect(host="localhost", port=3306,
                          user=argv[1], passwd=argv[2], db=argv[3])
         curSor = db.cursor()
-        query = "SELECT * FROM `states` WHERE `name`= %s \
+        query = "SELECT * FROM `states` WHERE `name`='{}' \
                        ORDER BY `id` ASC".format(argv[4])
         params = (argv[4])
         curSor.execute(query, params)
