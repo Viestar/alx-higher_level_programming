c97e4e8cd1c8429602422d0140fd8d713bf9718d
Viestar <m.silverbuckson@gmail.com>
2023-08-18 10:37:40 +0300

    Added .format()

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,6 +12,6 @@ if __name__ == "__main__":
     else:
         sys.exit(1)
     curSor = db.cursor()
-    curSor.execute(f"SELECT * FROM `states` WHERE `name` = '{argv[4]}'")
+    curSor.execute("SELECT * FROM `states` WHERE name = '{}'".format(argv[4]))
     [print(state) for state in curSor.fetchall()]
     curSor.close(), db.close()
