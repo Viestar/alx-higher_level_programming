3da6b935ee520a8204f2ff9660106506d8c76c8c
Viestar <m.silverbuckson@gmail.com>
2023-08-20 16:43:35 +0300

    Refined Query

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -11,11 +11,10 @@ if __name__ == "__main__":
                          user=argv[1], passwd=argv[2], db=argv[3])
         curSor = db.cursor()
         query = "SELECT * FROM cities INNER JOIN states ON \
-        WHERE cities.state_id = state.id ORDER BY cities.id ASC"
+        cities.state_id = state.id WHERE state.name=%s ORDER BY cities.id ASC"
         params = argv[4]
-        curSor.execute(query)
-        [print(", ".join(city[2])) for city in
-            curSor.fetchall() if city == params]
+        curSor.execute(query, params)
+        [print(", ".join(city[1])) for city in curSor.fetchall()]
         curSor.cloe(), db.close()
     else:
         sys.exit(1)
