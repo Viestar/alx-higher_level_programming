477db8ebd1e83343c495f395d0e514efc9b3711f
Viestar <m.silverbuckson@gmail.com>
2023-08-20 16:23:54 +0300

    5-filter_cities.py

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" script that lists all cities of a given state """
+
+import MySQLdb as Qdb
+import sys
+from sys import argv
+
+if __name__ == "__main__":
+    if len(argv) == 4:
+        db = Qdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+        curSor = db.cursor()
+        query = "SELECT cities.name FROM cities INNER JOIN states ON \
+        WHERE cities.state_id = state.id ORDER BY cities.id ASC"
+        params = argv[4]
+        curSor.execute(query)
+        [print(", ".join(city[2])) for city in
+            curSor.fetchall() if city == params]
+    else:
+        sys.exit(1)
