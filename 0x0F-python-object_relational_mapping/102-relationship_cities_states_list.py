209c5c6b4eeb0360d97fed5340a045f87392dd3c
Viestar <m.silverbuckson@gmail.com>
2023-08-21 20:36:33 +0300

    Added f on line 19

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
         Session = sessionmaker(bind=engine)
         session = Session()
 
-        [print(f"{city.id}: {city.name} -> {city.state.name}")
+        [print(f"{city[0]}: {city[1]} -> {city[2]}")
             for city in session.query(City).order_by(City.id)]
     else:
         sys.exit(1)
