d94656fc13addd2f53ca67f377bc2ef570189d80
Viestar <m.silverbuckson@gmail.com>
2023-08-21 08:44:12 +0300

    Revert "REfined query"
    This reverts commit 0b4e8858961109a03a68ae380fd33f18e5170102.

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -7,20 +7,16 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    db = Qdb.connect(
-            host="localhost",
-            port=3306,
-            user=argv[1],
-            passwd=argv[2],
-            db=argv[3]
-    )
-    curSor = db.cursor()
-    query = "SELECT cities.id, cities.name, states.name \
-            FROM cities JOIN states ON cities.state_id = states.id \
-            WHERE states.name = %s ORDER BY cities.id ASC"
-    param = argv[4]
-    curSor.execute(query, (param,))
-
-    [print(", ".join(city[1])) for city in curSor.fetchall()]
-    curSor.close()
-    db.close()
+    if len(argv) == 5:
+        db = Qdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+        curSor = db.cursor()
+        params = argv[4]
+        curSor.execute("""SELECT cities.id, cities.name, states.name
+            FROM cities INNER JOIN states ON cities.state_id = states.id
+            WHERE states.name=%s
+            ORDER BY id ASC""", (params,))
+        [print(", ".join(city[1])) for city in curSor.fetchall()]
+        curSor.cloe(), db.close()
+    else:
+        sys.exit(1)
