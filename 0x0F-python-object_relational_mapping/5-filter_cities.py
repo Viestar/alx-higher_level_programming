f3872d5a74365111ab89b6a786342191d19f2e83
Viestar <m.silverbuckson@gmail.com>
2023-08-21 08:23:46 +0300

    Try another solution

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -5,25 +5,25 @@ import MySQLdb as Qdb
 import sys
 from sys import argv
 
+
 if __name__ == "__main__":
-    if len(argv) == 5:
-        ur = argv[1]
-        pd = argv[2]
-        de = argv[3]
-        se = argv[4]
-        cs = []
-        db = Qdb.connect(host="localhost", port=3306,
-                         user=ur, passwd=pd, db=de)
-        curSor = db.cursor()
-        curSor.execute("""SELECT cities.id, cities.name, states.name
-            FROM cities INNER JOIN states ON cities.state_id = states.id
-            WHERE states.name=%s
-            ORDER BY id ASC""", (se,))
-        rows = curSor.fetchall()
-        for row in rows:
-            cs.append(row[1])
-            print(', '.join(row))
-            curSor.close()
-            db.close()
-    else:
-        sys.exit(1)
+    db = Qdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
+    )
+    cursor = db.cursor()
+    query = "SELECT cities.id, cities.name, states.name \
+            FROM cities JOIN states ON cities.state_id = states.id \
+            WHERE states.name = %s ORDER BY cities.id ASC"
+    param = argv[4]
+    cursor.execute(query, (param,))
+    city_rows = cursor.fetchall()
+
+    cities = [city[1] for city in city_rows]
+    formated_city_name = ", ".join(cities)
+    print(formated_city_name)
+    cursor.close()
+    db.close()
