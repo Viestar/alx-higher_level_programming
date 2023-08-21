a035b75834a16d583bdb87076808bfabb49cf3c9
Viestar <m.silverbuckson@gmail.com>
2023-08-21 07:03:10 +0300

    Corrected no. of arguments

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -6,7 +6,7 @@ import sys
 from sys import argv
 
 if __name__ == "__main__":
-    if len(argv) == 4:
+    if len(argv) == 5:
         db = Qdb.connect(host="localhost", port=3306,
                          user=argv[1], passwd=argv[2], db=argv[3])
         curSor = db.cursor()
