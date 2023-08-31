0fec0cab0b02793efe05ad9d9f5c086a3145ec0d
Viestar <m.silverbuckson@gmail.com>
2023-08-31 18:09:49 +0300

    6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """ Peak integer identifier """
 
 
@@ -8,4 +8,5 @@ def find_peak(list_of_integers):
     local_list.sort()
     if local_list:
         return local_list[-1]
-    return None
+    else:
+        return None
