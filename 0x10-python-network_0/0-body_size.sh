8ff1f3dd6cacbdb2b3231c03a612a5e32c6d0885
Viestar <m.silverbuckson@gmail.com>
2023-08-31 13:51:35 +0300

    Refined the condition

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # A Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
 
-if [ "$#" -eq 2 ]; then
-    size=$(curl -s "$1" | wc -c)
-    echo "$size"
+if [ "$#" -gt 0 ]; then
+  size=$(curl -s "$1" | wc -c)
+  echo "$size"
 else
-    exit 1
+  exit 1
 fi
