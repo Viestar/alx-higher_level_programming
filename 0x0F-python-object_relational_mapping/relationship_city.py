f0b6b27b8cefbe3075179537de37871b2bd5df41
Viestar <m.silverbuckson@gmail.com>
2023-09-02 16:09:03 +0300

    reverts

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,33 +1,18 @@
 #!/usr/bin/python3
 """ python file defining State class from Base creating table states """
 
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from relationship_state import Base, State
-from relationship_city import City
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
+Base = declarative_base()
 
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        username, password, database), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+class City(Base):
+    """ City class creating states table inheriting from the Base class """
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    __tablename__ = "cities"
 
-    new_state = State(name="California")
-    new_city = City(name="San Francisco")
-    new_state.cities.append(new_city)
-
-    session.add(new_state)
-    session.commit()
-
-    session.close()
+    id = Column(Integer, primary_key=True,
+                unique=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
 """ python file defining State class from Base creating table states """
 
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-from model_city import Base, City
+
+Base = declarative_base()
 
 
 class State(Base):
-    """State class"""
-    __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    """ State class creating states table inheriting from the Base class """
+
+    __tablename__ = "states"
+
+    id = Column(Integer, primary_key=True,
+                unique=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state",
                           cascade="all, delete-orphan")
