97bb3ca02f36a4f7f9252079d53403146781bcb6
Viestar <m.silverbuckson@gmail.com>
2023-09-02 09:58:55 +0300

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,24 +1,12 @@
 #!/usr/bin/python3
 """ Script that fetches https://alx-intranet.hbtn.io/status with urllib """
 
-import urllib.request
-
+import urllib
 
 if __name__ == "__main__":
-    request = urllib.request.Request("https://intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
-# from urllib import request
-
-# if __name__ == "__main__":
-#     data = request.Request('https://intranet.hbtn.io/status')
-#     with request.urlopen(data) as pool:
-#         html = pool.read()
-#         print('Body response:')
-#         print(f"\t- type: {type(html)}")
-#         print(f"\t- content: {html}")
-#         print(f"\t- utf8 content: {html.decode('utf-8')}")
+    response = urllib.request.Request('https://intranet.hbtn.io/status')
+    with urllib.request.urlopen(response) as data:
+        print('Body response:')
+        print(f"\t- type: {type(data.read())}")
+        print(f"\t- content: {data.read()}")
+        print(f"\t- utf8 content: {(data.read()).decode('utf-8')}")
