244d50567b054f41a7d0831076e7ca738c6a8981
Viestar <m.silverbuckson@gmail.com>
2023-09-02 07:57:00 +0300

    3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -6,18 +6,26 @@ and displays the body of the response (decoded in utf-8)
 
 from sys import argv
 import sys
-from urllib.request import urlopen, Request
-from urllib.error import HTTPError
+import urllib.request
+import urllib.error
+if __name__ == "__main__":
+    url = sys.argv[1]
 
-if __name__ == '__main__':
-    if len(argv) > 0:
-        data_url = Request(argv[1])
-        try:
-            with urlopen(data_url) as data:
-                html = data.read()
-                utf8 = html.decode('utf-8')
-                print(utf8)
-        except HTTPError:
-            print(f"Error code: {HTTPError.code}")
-    else:
-        sys.exit()
+    request = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode("ascii"))
+    except urllib.error.HTTPError as err:
+        print("Error code: {}".format(err.code))
+# if __name__ == '__main__':
+#     if len(argv) > 0:
+#         data_url = Request(argv[1])
+#         try:
+#             with urlopen(data_url) as data:
+#                 html = data.read()
+#                 utf8 = html.decode('utf-8')
+#                 print(utf8)
+#         except HTTPError :
+#             print(f"Error code: {HTTPError.code}")
+#     else:
+#         sys.exit()
