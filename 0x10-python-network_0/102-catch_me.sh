c23d5db437e92daa1f7a38af11891fa198eb72c2
Viestar <m.silverbuckson@gmail.com>
2023-09-06 15:32:10 +0300

    102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response
-curl -sL 0.0.0.0:5000/catch_me_3 -X PUT -H "Origin:Holberton school"
+curl -s 0.0.0.0:5000/catch_me tee /dev/null | grep -o 'You got me!'
